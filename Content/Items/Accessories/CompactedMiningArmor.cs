34dbffc4554ab3ce519f402e80ebe0c776797686
Mathieu Gauvin-Gingras <gauvingingras@gmail.com>
2025-12-05 11:46:58 -0500

    added Compacted Mining Armor

---
+++ Content/Items/Accessories/CompactedMiningArmor.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Compactor.Content.Items.Accessories {
+    public class CompactedMiningArmor : ModItem {
+
+
+        public override void SetDefaults() {
+            Item.DefaultToAccessory(32, 32);
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.sellPrice(gold: 1);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual) {
+            player.pickSpeed -= 0.3f;
+            base.UpdateAccessory(player, hideVisual);
+        }
+
+        public override void AddRecipes() {
+            CreateRecipe()
+                .AddIngredient(ItemID.MiningHelmet)
+                .AddIngredient(ItemID.MiningShirt)
+                .AddIngredient(ItemID.MiningPants)
+                .AddTile(TileID.TinkerersWorkbench)
+                .Register();
+        }
+    }
+}
